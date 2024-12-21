af707a2b59b8f57ef86bd9699b45683620a560dd
tusharmeno <tusharmenon6@gmail.com>
2024-12-21 20:02:46 +0530

    initialcommit

---
+++ python2.py
@@ -0,0 +1,9 @@
+A = input("Enter first number")
+B = input("Enter second number")
+if A > B:
+    print("A greater than B")
+if A < B:
+    print("B greater than A")
+if A == B:
+    print("A equal to B")
+print("Sum of A and B ="+(A+B))
